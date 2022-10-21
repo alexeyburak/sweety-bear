0794e07730d7ac120d4ecd059e932eb6116b7c46
Alexey Burak <byraklesha@gmail.com>
2022-10-21 20:37:50 +0300

    init: build project

---
+++ src/main/java/by/bsuir/sweetybear/Runner.java
@@ -0,0 +1,15 @@
+package by.bsuir.sweetybear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Runner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Runner.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsuir/sweetybear/SweetyBearApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.sweetybear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetyBearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
