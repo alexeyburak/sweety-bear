9eecfed942a93dd99e2a53ad1cc2c108e45cf725
cookieemoon <tvladimirovnaaa@mail.ru>
2022-12-09 22:12:19 +0300

    docs: add javadocs to pdf export service

--- src/main/java/by/bsuir/sweetybear/service/PDFGeneratorService.java
+++ src/main/java/by/bsuir/sweetybear/service/PDFGeneratorService.java
@@ -10,6 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface PDFGeneratorService {
 
+    /**
+     * <p>Export user list in PDF</p>
+     * @param response HttpServletResponse
+     * @since 1.0
+     */
     void exportUsersInPDF(HttpServletResponse response);
+    /**
+     * <p>Export user orders in PDF</p>
+     * @param response HttpServletResponse
+     * @param orderId Order id to export
+     * @since 1.0
+     */
     void exportUserOrderInPDF(HttpServletResponse response, Long orderId);
 }
