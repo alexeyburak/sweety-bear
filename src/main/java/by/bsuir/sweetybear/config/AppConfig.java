12debc6785e211fa88d4ce9f2e5ba3c6201191ba
Alexey Burak <byraklesha@gmail.com>
2022-12-12 20:08:09 +0300

    feat: add locale switcher

--- src/main/java/by/bsuir/sweetybear/config/AppConfig.java
+++ src/main/java/by/bsuir/sweetybear/config/AppConfig.java
@@ -3,6 +3,10 @@ package by.bsuir.sweetybear.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+
+import java.util.Locale;
 
 /**
  * sweety-bear
@@ -16,5 +20,13 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(new Locale("en"));
+        cookieLocaleResolver.setCookieName("language");
+        return cookieLocaleResolver;
     }
 }
