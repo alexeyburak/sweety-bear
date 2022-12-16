b42901a8b3482d680d7a43efb1fb7ef6fe569563
Alexey Burak <byraklesha@gmail.com>
2022-12-16 17:52:01 +0300

    feat: init password reset service

---
+++ src/main/java/by/bsuir/sweetybear/service/impl/ForgotPasswordServiceImpl.java
@@ -0,0 +1,66 @@
+package by.bsuir.sweetybear.service.impl;
+
+import by.bsuir.sweetybear.model.User;
+import by.bsuir.sweetybear.repository.UserRepository;
+import by.bsuir.sweetybear.service.ForgotPasswordService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+/**
+ * sweety-bear
+ * Created by Alexey Burak
+ * Dec 2022
+ */
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ForgotPasswordServiceImpl implements ForgotPasswordService {
+
+    @Value("${server.port}")
+    private String serverPort;
+    private final UserServiceImpl userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final MailSenderImpl mailSender;
+
+    public boolean setCodeToResetUserPassword(String email) {
+        User user = userService.getUserByEmail(email);
+        if (user == null) return false;
+
+        user.setResetPasswordCode(UUID.randomUUID().toString());
+        sendResetPasswordLinkToUser(user);
+        log.warn("Send email to reset user password.");
+
+        return true;
+    }
+
+    private void sendResetPasswordLinkToUser(final User user) {
+        String message = String.format(
+                "%s, click link to set new password" +
+                        "\nActivate your email: http://localhost:" + serverPort + "/reset/%s ",
+                user.getName(),
+                user.getResetPasswordCode()
+        );
+        String title = "Reset password";
+
+        mailSender.send(user.getEmail(), title, message);
+    }
+
+    public boolean changeUserPassword(String resetPasswordCode, String updatedPassword) {
+        User user = userRepository.findByResetPasswordCode(resetPasswordCode);
+        if (user == null) return false;
+
+        user.setPassword(passwordEncoder.encode(updatedPassword));
+        user.setResetPasswordCode(null);
+
+        userService.save(user);
+        log.info("User reset password. User email: {}", user.getEmail());
+        return true;
+    }
+}
