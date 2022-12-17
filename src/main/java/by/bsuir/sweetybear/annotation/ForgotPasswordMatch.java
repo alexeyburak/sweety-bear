d557a58d97fbd1660adde97ade1c24b0ac4aca32
Alexey Burak <byraklesha@gmail.com>
2022-12-17 15:15:11 +0300

    feat: add reseting password validation

---
+++ src/main/java/by/bsuir/sweetybear/annotation/ForgotPasswordMatch.java
@@ -0,0 +1,30 @@
+package by.bsuir.sweetybear.annotation;
+
+import by.bsuir.sweetybear.validator.ErrorMessage;
+import by.bsuir.sweetybear.validator.impl.ForgotPasswordMatchValidatorImpl;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * sweety-bear
+ * Created by Alexey Burak
+ * Dec 2022
+ */
+
+@Target(TYPE)
+@Retention(RUNTIME)
+@Constraint(validatedBy= ForgotPasswordMatchValidatorImpl.class)
+@Documented
+public @interface ForgotPasswordMatch {
+    String message() default ErrorMessage.PASSWORDS_NOT_MATCH;
+
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
