2f712f9ed21d0f0c70325bb480669b75872312f1
Alexey Burak <byraklesha@gmail.com>
2022-12-19 20:14:53 +0300

    feat: add oauth2 service impl

--- src/main/java/by/bsuir/sweetybear/config/security/WebSecurityConfig.java
+++ src/main/java/by/bsuir/sweetybear/config/security/WebSecurityConfig.java
@@ -1,16 +1,27 @@
 package by.bsuir.sweetybear.config;
 
-import by.bsuir.sweetybear.service.impl.CustomUserDetailsService;
+import by.bsuir.sweetybear.service.impl.CustomOauth2UserServiceImpl;
+import by.bsuir.sweetybear.service.impl.CustomUserDetailsServiceImpl;
+import by.bsuir.sweetybear.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * sweety-bear
@@ -21,15 +32,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailsServiceImpl userDetailsService;
+    private final CustomOauth2UserServiceImpl oauth2UserService;
+    private final Oauth2LoginSuccessHandler oauth2LoginSuccessHandler;
 
-    private final CustomUserDetailsService userDetailsService;
+    public WebSecurityConfig(CustomUserDetailsServiceImpl userDetailsService,
+                             CustomOauth2UserServiceImpl oauth2UserService,
+                             @Lazy Oauth2LoginSuccessHandler oauth2LoginSuccessHandler) {
+        this.userDetailsService = userDetailsService;
+        this.oauth2UserService = oauth2UserService;
+        this.oauth2LoginSuccessHandler = oauth2LoginSuccessHandler;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                    .antMatchers("/oauth2/**")
+                    .permitAll()
                     .antMatchers("/", "/registration", "/css/**", "/js/**", "/images/**", "/activate/*", "/forgot_password", "/reset_password/*", "/reset_password")
                     .permitAll()
                     .anyRequest()
@@ -39,6 +60,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .loginPage("/login")
                     .failureForwardUrl("/login/error")
                     .permitAll()
+                .and()
+                    .oauth2Login()
+                    .loginPage("/login")
+                    .userInfoEndpoint()
+                    .userService(oauth2UserService)
+                    .and()
+                    .successHandler(oauth2LoginSuccessHandler)
                 .and()
                     .logout()
                     .permitAll();
