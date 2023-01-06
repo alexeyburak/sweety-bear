9f64ce3bda7a1ca55bfd2a6191ac9cca59446e69
Alexey Burak <byraklesha@gmail.com>
2023-01-06 17:21:35 +0300

    feat: add feedback sorting function

--- src/main/java/by/bsuir/sweetybear/service/FeedbackService.java
+++ src/main/java/by/bsuir/sweetybear/service/FeedbackService.java
@@ -2,6 +2,7 @@ package by.bsuir.sweetybear.service;
 
 import by.bsuir.sweetybear.model.Feedback;
 import by.bsuir.sweetybear.model.User;
+import by.bsuir.sweetybear.model.enums.FeedbackSortType;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import java.util.List;
 public interface FeedbackService {
 
     List<Feedback> getProductFeedbackList(long productId);
+    List<Feedback> getProductFeedbackListWithSortingType(long productId, FeedbackSortType sortType);
     void addFeedback(User user, Long productId, Feedback feedback);
     void deleteFeedback(Long id);
 }
--- src/main/java/by/bsuir/sweetybear/service/impl/FeedbackServiceImpl.java
+++ src/main/java/by/bsuir/sweetybear/service/impl/FeedbackServiceImpl.java
@@ -2,6 +2,7 @@ package by.bsuir.sweetybear.service.impl;
 
 import by.bsuir.sweetybear.model.Feedback;
 import by.bsuir.sweetybear.model.User;
+import by.bsuir.sweetybear.model.enums.FeedbackSortType;
 import by.bsuir.sweetybear.repository.FeedbackRepository;
 import by.bsuir.sweetybear.service.FeedbackService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -26,11 +28,28 @@ public class FeedbackServiceImpl implements FeedbackService {
     private final ProductServiceImpl productService;
 
     @Override
-    public List<Feedback> getProductFeedbackList(long productId) {
-        return feedbackRepository.getFeedbacksByProductId(productId)
-                .stream()
-                .sorted(Feedback::compareTo)
-                .toList();
+    public List<Feedback> getProductFeedbackList(long id) {
+        return feedbackRepository.getFeedbacksByProductId(id);
+    }
+
+    @Override
+    public List<Feedback> getProductFeedbackListWithSortingType(long id, FeedbackSortType sortType) {
+        final List<Feedback> feedbacksByProductId = this.getProductFeedbackList(id);
+
+        if (sortType != null) {
+            switch (sortType) {
+                case RATING_ASC -> feedbacksByProductId
+                        .sort(Comparator.comparing(Feedback::getStars));
+                case RATING_DESC -> feedbacksByProductId
+                        .sort(Comparator.comparing(Feedback::getStars).reversed());
+                case DATE_ASC -> feedbacksByProductId
+                        .sort(Comparator.comparing(Feedback::getDateOfCreated));
+                case DATE_DESC -> feedbacksByProductId
+                        .sort(Comparator.comparing(Feedback::getDateOfCreated).reversed());
+            }
+        }
+
+        return feedbacksByProductId;
     }
 
     @Override
