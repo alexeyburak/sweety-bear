c9544921985a263a877372a0ae567b4242e1182b
Alexey Burak <byraklesha@gmail.com>
2023-01-06 16:15:09 +0300

    feat: add feedback sorting types

---
+++ src/main/java/by/bsuir/sweetybear/model/enums/FeedbackSortType.java
@@ -0,0 +1,12 @@
+package by.bsuir.sweetybear.model.enums;
+
+/**
+ * sweety-bear
+ * Created by Alexey Burak
+ * Jan 2023
+ */
+
+public enum FeedbackSortType {
+    RATING_ASC, RATING_DESC, DATE_ASC, DATE_DESC
+
+}
