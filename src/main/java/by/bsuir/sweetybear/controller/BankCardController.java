1d9da966e792d9852af8a7f910bc54bf928549df
Alexey Burak <byraklesha@gmail.com>
2023-01-10 21:08:54 +0300

    feat: separate bank card controller

---
+++ src/main/java/by/bsuir/sweetybear/controller/BankCardController.java
@@ -0,0 +1,61 @@
+package by.bsuir.sweetybear.controller;
+
+import by.bsuir.sweetybear.dto.BankCardDTO;
+import by.bsuir.sweetybear.model.User;
+import by.bsuir.sweetybear.service.impl.BankCardServiceImpl;
+import by.bsuir.sweetybear.service.impl.UserServiceImpl;
+import by.bsuir.sweetybear.validator.ErrorMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+/**
+ * sweety-bear
+ * Created by Alexey Burak
+ * Jan 2023
+ */
+
+@Controller
+@RequiredArgsConstructor
+public class BankCardController {
+
+    private final UserServiceImpl userService;
+    private final BankCardServiceImpl bankCardService;
+
+    @GetMapping("/add/payment")
+    public String addBankCardForm(@ModelAttribute("bankCard") BankCardDTO bankCard,
+                                  Principal principal,
+                                  Model model) {
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
+        return "bank-card-add";
+    }
+
+    @PostMapping("/add/payment")
+    public String addBankCard(@ModelAttribute("bankCard") BankCardDTO bankCard,
+                              Principal principal,
+                              Model model) {
+        User user = userService.getUserByPrincipal(principal);
+
+        if (!bankCardService.addNewBankCard(bankCard, user)) {
+            model.addAttribute("user", user);
+            model.addAttribute("report", ErrorMessage.ADD_BANK_CARD_ERROR);
+            return "bank-card-add";
+        }
+
+        return "redirect:/user/edit/" + user.getId() + "?tab=payments";
+    }
+
+    @PostMapping("/user/delete/payment/{id}")
+    public String deleteUserBankCard(@PathVariable Long id,
+                                     HttpServletRequest request) {
+        bankCardService.deleteBankCardById(id);
+        return "redirect:" + request.getHeader("Referer");
+    }
+}
