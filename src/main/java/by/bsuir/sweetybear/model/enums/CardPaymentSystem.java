61b94fd3904cd44c99fc03fc8c34fe03934c22a2
Alexey Burak <byraklesha@gmail.com>
2023-01-28 17:45:54 +0300

    feat: add payment systems enum

---
+++ src/main/java/by/bsuir/sweetybear/model/enums/CardPaymentSystem.java
@@ -0,0 +1,11 @@
+package by.bsuir.sweetybear.model.enums;
+
+/**
+ * sweety-bear
+ * Created by Alexey Burak
+ * Jan 2023
+ */
+
+public enum CardPaymentSystem {
+    VISA, MASTERCARD, AMERICAN_EXPRESS
+}
