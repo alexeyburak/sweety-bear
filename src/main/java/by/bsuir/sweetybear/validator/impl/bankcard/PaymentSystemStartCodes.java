ec039644507ec57dc57e850d390f1c2b3205634e
Alexey Burak <byraklesha@gmail.com>
2023-01-30 13:00:13 +0300

    feat: payment systems start codes

---
+++ src/main/java/by/bsuir/sweetybear/validator/impl/bankcard/PaymentSystemStartCodes.java
@@ -0,0 +1,13 @@
+package by.bsuir.sweetybear.validator.impl.bankcard;
+
+/**
+ * sweety-bear
+ * Created by Alexey Burak
+ * Jan 2023
+ */
+
+public interface PaymentSystemStartCodes {
+    String VISA_START_WITH = "4";
+    String[] MASTERCARD_START_WITH = {"2", "5"};
+    String AMERICAN_EXPRESS_START_WITH = "3";
+}
