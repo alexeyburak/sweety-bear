a1e643b45d684f193ba54af241a82d5d2161f403
Alexey Burak <byraklesha@gmail.com>
2023-01-31 23:43:48 +0300

    fix: convert api exception to record

--- src/main/java/by/bsuir/sweetybear/exception/ApiException.java
+++ src/main/java/by/bsuir/sweetybear/exception/ApiException.java
@@ -1,6 +1,5 @@
 package by.bsuir.sweetybear.exception;
 
-import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
@@ -11,20 +10,8 @@ import java.time.ZonedDateTime;
  * Nov 2022
  */
 
-@Data
-public class ApiException {
-    private final String message;
-    private final Throwable throwable;
-    private final HttpStatus httpStatus;
-    private final ZonedDateTime timestamp;
-
-    public ApiException(String message,
-                        Throwable throwable,
-                        HttpStatus httpStatus,
-                        ZonedDateTime timestamp) {
-        this.message = message;
-        this.throwable = throwable;
-        this.httpStatus = httpStatus;
-        this.timestamp = timestamp;
-    }
-}
+public record ApiException(String message,
+                           Throwable throwable,
+                           HttpStatus httpStatus,
+                           ZonedDateTime timestamp
+) {}
