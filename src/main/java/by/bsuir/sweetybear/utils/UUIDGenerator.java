63b58e8343a505e2afa7fc9eb6036f4a48125045
Alexey Burak <byraklesha@gmail.com>
2023-02-03 18:24:55 +0300

    feat: add uuid generator

---
+++ src/main/java/by/bsuir/sweetybear/utils/UUIDGenerator.java
@@ -0,0 +1,15 @@
+package by.bsuir.sweetybear.utils;
+
+import java.util.UUID;
+
+/**
+ * sweety-bear
+ * Created by Alexey Burak
+ * Feb 2023
+ */
+
+public class UUIDGenerator {
+    public static String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+}
