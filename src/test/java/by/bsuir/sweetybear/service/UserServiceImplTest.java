852663c689b0e9d60bd053662535068d9ee39347
Alexey Burak <byraklesha@gmail.com>
2023-02-05 12:27:11 +0300

    test: split functions and fix names in user service test

--- src/test/java/by/bsuir/sweetybear/service/UserServiceImplTest.java
+++ src/test/java/by/bsuir/sweetybear/service/UserServiceImplTest.java
@@ -1,10 +1,11 @@
-package by.bsuir.sweetybear.service.impl;
+package by.bsuir.sweetybear.service;
 
 import by.bsuir.sweetybear.model.Product;
 import by.bsuir.sweetybear.model.User;
 import by.bsuir.sweetybear.model.enums.Role;
 import by.bsuir.sweetybear.repository.UserRepository;
-import org.aspectj.util.Reflection;
+import by.bsuir.sweetybear.service.impl.MailSenderImpl;
+import by.bsuir.sweetybear.service.impl.UserServiceImpl;
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,8 +15,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,7 +24,7 @@ import static by.bsuir.sweetybear.utils.UUIDGenerator.generateUUID;
 import static org.mockito.Mockito.doNothing;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceImplTest {
+class UserServiceImplTest {
     @Mock
     private MailSenderImpl mailSender;
     @Mock
@@ -36,7 +35,7 @@ public class UserServiceImplTest {
     private UserServiceImpl userService;
 
     @Test
-    public void checkAddUser() {
+    void addUserToDatabase_NotExistsUser_True() {
         // given
         String code = generateUUID();
         String email = "burakalexey@yahoo.com";
@@ -52,7 +51,7 @@ public class UserServiceImplTest {
         Mockito.when(userRepository.findByEmail(email)).thenReturn(null);
         Mockito.when(passwordEncoder.encode(code)).thenReturn(code);
         doNothing().when(mailSender).sendEmailWithActivationLinkToUser(user);
-        userService.addUserToDatabase(user);
+        boolean result = userService.addUserToDatabase(user);
 
         //then
         ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
@@ -60,11 +59,11 @@ public class UserServiceImplTest {
         User capturedUser = userArgumentCaptor.getValue();
         Assertions.assertNotNull(capturedUser);
         AssertionsForClassTypes.assertThat(capturedUser).isEqualTo(user);
-
+        Assertions.assertTrue(result);
     }
 
     @Test
-    public void getUserByResetCode() {
+    void getUserByResetPasswordCode_UserWithResetCode_ShouldReturnUserMatchesResetCode() {
         //given
         String resetCode = generateUUID();
         User user = User.builder()
@@ -76,13 +75,12 @@ public class UserServiceImplTest {
         User userFromDatabase = userService.getUserByResetPasswordCode(resetCode);
 
         //then
-        Mockito.verify(userRepository, Mockito.times(1)).findByResetPasswordCode(resetCode);
         Assertions.assertNotNull(userFromDatabase);
         Assertions.assertEquals(user, userFromDatabase);
     }
 
     @Test
-    public void banUserById() {
+    void banUserAccountById_ActiveUser_ShouldChangeUserActivity() {
         //given
         User user = User.builder()
                 .active(true)
@@ -90,21 +88,15 @@ public class UserServiceImplTest {
 
         //when
         Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-
-        //then
-        Assertions.assertTrue(user.isActive());
-
-        //when
         userService.banUserAccountById(1L);
 
         //then
-        Mockito.verify(userRepository, Mockito.times(1)).findById(1L);
         Assertions.assertNotNull(user);
         Assertions.assertFalse(user.isActive());
     }
 
     @Test
-    public void updateUserDataById() {
+    void updateUserDataById_UpdatedUserAndEditedUserId_ShouldUpdateEditedUserData() {
         //given
         User user = User.builder()
                 .email("burakalexey@yahoo.com")
@@ -128,7 +120,7 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void changeUserRole() {
+    void changeUserRole_UserRoleUser_ShouldChangeForRoleAdmin() {
         //given
         User user = User.builder()
                 .roles(new HashSet<>(Collections.singletonList(Role.ROLE_USER)))
@@ -141,6 +133,14 @@ public class UserServiceImplTest {
         Assertions.assertNotNull(user);
         Assertions.assertEquals(1, user.getRoles().size());
         Assertions.assertEquals(Set.of(Role.ROLE_ADMIN), user.getRoles());
+    }
+
+    @Test
+    void changeUserRole_UserRoleAdmin_ShouldChangeForRoleUser() {
+        //given
+        User user = User.builder()
+                .roles(new HashSet<>(Collections.singletonList(Role.ROLE_ADMIN)))
+                .build();
 
         //when
         userService.changeUserRole(user);
@@ -152,7 +152,7 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void activateUserAccount() {
+    void activateUserAccountAfterRegistration_FindUserByActivationCode_ShouldChangeUserActivitySetCodeNullAndReturnTrue() {
         //given
         String activationCode = generateUUID();
         User user = User.builder()
@@ -165,14 +165,40 @@ public class UserServiceImplTest {
         userService.activateUserAccountAfterRegistration(activationCode);
 
         //then
-        Mockito.verify(userRepository, Mockito.times(1)).findByActivationCode(activationCode);
         Assertions.assertNotNull(user);
         Assertions.assertTrue(user.isActive());
         Assertions.assertNull(user.getActivationCode());
     }
 
     @Test
-    public void addProductToFavoritesAndRemoveIfExistsTest() {
+    void addProductToFavoritesAndRemoveIfExists_ExistsProduct_ShouldRemoveFromList() {
+        //given
+        Product firstProduct = createProduct(1L);
+        Product secondProduct = createProduct(2L);
+        String email = "burakalexey@yahoo.com";
+        User user = User.builder()
+                .email(email)
+                .favoriteProducts(new ArrayList<>(
+                        List.of(
+                                firstProduct,
+                                secondProduct
+                        )
+                ))
+                .build();
+
+
+        //when
+        Mockito.when(userRepository.findByEmail(email)).thenReturn(user);
+        userService.addProductToFavoritesAndRemoveIfExists(email, firstProduct);
+
+        //then
+        Assertions.assertNotNull(user.getFavoriteProducts());
+        Assertions.assertEquals(1, user.getFavoriteProducts().size());
+        Assertions.assertEquals(2L, user.getFavoriteProducts().get(0).getId());
+    }
+
+    @Test
+    void addProductToFavoritesAndRemoveIfExists_NotExistsProduct_ShouldAddProductToList() {
         //given
         Product firstProduct = createProduct(1L);
         Product secondProduct = createProduct(2L);
@@ -191,21 +217,12 @@ public class UserServiceImplTest {
 
         //when
         Mockito.when(userRepository.findByEmail(email)).thenReturn(user);
-        Assertions.assertEquals(2, user.getFavoriteProducts().size());
-        userService.addProductToFavoritesAndRemoveIfExists(email, firstProduct);
-
-        //then
-        Assertions.assertNotNull(user.getFavoriteProducts());
-        Assertions.assertEquals(1, user.getFavoriteProducts().size());
-        Assertions.assertEquals(2L, user.getFavoriteProducts().get(0).getId());
-
-        //when
         userService.addProductToFavoritesAndRemoveIfExists(email, thirdProduct);
 
         //then
         Assertions.assertNotNull(user.getFavoriteProducts());
-        Assertions.assertEquals(2, user.getFavoriteProducts().size());
-        Assertions.assertEquals(3L, user.getFavoriteProducts().get(1).getId());
+        Assertions.assertEquals(3, user.getFavoriteProducts().size());
+        Assertions.assertEquals(1L, user.getFavoriteProducts().get(0).getId());
     }
 
     private Product createProduct(Long id) {
@@ -215,7 +232,7 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void updateUserPasswordById() {
+    void updateUserPasswordById_UpdatedUserAndIdEditedUser_ShouldChangeUserPassword() {
         //given
         final String password = "password";
         final String newPassword = "newPassword";
@@ -229,12 +246,11 @@ public class UserServiceImplTest {
         //when
         Mockito.when(passwordEncoder.encode(newPassword)).thenReturn(newPassword);
         Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(user));
-        Assertions.assertEquals(password, user.getPassword());
         userService.updateUserPasswordById(1L, updatedUser);
 
         //then
         Assertions.assertNotNull(user);
+        Assertions.assertNotNull(user.getPassword());
         Assertions.assertEquals(newPassword, user.getPassword());
     }
-
 }
